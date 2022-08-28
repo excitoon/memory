98335049ad774dd994bd13d94457910ee79032a8
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-28 08:18:50 +0300

    Updated keywords and classifiers.

--- setup.py
+++ setup.py
@@ -13,16 +13,25 @@ with open(f"{os.path.dirname(os.path.abspath(__file__))}/README.md") as readme:
         author_email="vladimir.chebotarev@gmail.com",
         license="MIT",
         classifiers=[
-            "Development Status :: 5 - Production/Stable",  # FIXME
+            "Development Status :: 5 - Production/Stable",
             "Environment :: Console",
             "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: System Administrators",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "Topic :: Scientific/Engineering",
             "Topic :: Software Development",
+            "Topic :: System :: Benchmark",
             "Topic :: Utilities",
         ],
-        keywords=[],  # FIXME
+        keywords=["benchmark", "cpu", "ram", "time", "memory", "heap", "profiling", "valgrind", "massif"],
         project_urls={
             "Documentation": "https://github.com/excitoon/memory/blob/master/README.md",
             "Source": "https://github.com/excitoon/memory",
